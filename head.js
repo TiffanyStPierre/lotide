7649b552382b9d6fb52da83c127da2e8ac702a85
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-10-16 18:39:14 -0600

    Added tail function and linted head function

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -8,9 +8,9 @@ const assertEqual = function (actual, expected) {
 
 // FUNCTION IMPLEMENTATION
 
-const head = function (arr) {
+const head = function(arr) {
   return arr[0];
-}
+};
 
 // TEST CODE
 assertEqual(head([5, 6, 7]), 5);
---
+++ tail.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+// TEST CODE
+
+// Test Case 1:
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2:
+const result2 = tail(["Hello"]);
+assertEqual(result2.length, 0);
+
+// Test Case 2:
+const result3 = tail([]);
+assertEqual(result3.length, 0);
