ef64ea5bb9f0c0dd04491295939f8e354258f645
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-10-18 07:48:50 -0600

    Changed middle function line 43 to check for odd numbers

--- middle.js
+++ middle.js
@@ -40,7 +40,7 @@ const middle = function(arr) {
     middleElements.push(arr[Math.floor(arr.length / 2)- 1]);
     middleElements.push(arr[Math.floor(arr.length / 2)]);
     return middleElements;
-  }else if (arr.length > 2 && arr.length % 1 === 0) {
+  }else if (arr.length > 2 && arr.length % 2 !== 0) {
     middleElements.push(arr[Math.floor(arr.length / 2)]);
     return middleElements;
   }
@@ -49,22 +49,22 @@ const middle = function(arr) {
 // TESTS 
 
 const test1 = middle([2, 6, 8, 10, 13, 24]);
-console.log(assertArraysEqual(test1, [8, 10]));
+assertArraysEqual(test1, [8, 10]);
 
 const test2 = middle([2, 5, 8, 12, 24]);
-console.log(assertArraysEqual(test2, [8]));
+assertArraysEqual(test2, [8]);
 
 const test3 = middle([]);
-console.log(assertArraysEqual(test3, []));
+assertArraysEqual(test3, []);
 
 const test4 = middle([2]);
-console.log(assertArraysEqual(test4, []));
+assertArraysEqual(test4, []);
 
 const test5 = middle([13, 24]);
-console.log(assertArraysEqual(test5, []));
+assertArraysEqual(test5, []);
 
 const test6 = middle([2, 16, 65, 120, 89, 6, 3, 900, 12, 6, 8, 10, 13, 24]);
-console.log(assertArraysEqual(test6, [3, 900]));
+assertArraysEqual(test6, [3, 900]);
 
 const test7 = middle([2, 16, 65, 120, 89, 6, 3, 900, 16, 19, 87, 12, 6, 8, 10, 13, 24]);
-console.log(assertArraysEqual(test7, [16]));
+assertArraysEqual(test7, [16]);
