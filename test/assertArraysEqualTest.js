907294eac586b516525bbb200b4a2f9e5a60d96b
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-10-23 06:22:41 -0600

    added test cases to assertArraysEqualTest

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,1 +1,10 @@
 const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual(["dog", "cat", "monkey"], ["dog", "cat", "monkey"]); // => should PASS
+assertArraysEqual([1, 2, 3], ["1", "2", "3"]); // => should NOT PASS
+assertArraysEqual([1, 2, 3], ["dog", "cat", "monkey"]); // => should NOT PASS
+assertArraysEqual([1, 2, 3], [12, 2, 3]); // => should NOT PASS
+assertArraysEqual([1, 2, 3], [1, 12, 3]); // => should NOT PASS
+assertArraysEqual([1, 2, 3], [2, 2, 13]); // => should NOT PASS
+assertArraysEqual(["dog", "cat", "monkey"], ["cat", "dog", "monkey"]); // => should NOT PASS
